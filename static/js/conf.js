fb6ed4eb14dfac03dbf87d37b80632f2230e7e7c
ansonchen <83655528@qq.com>
2016-10-12 23:17:43 +0800

    同步线上版本

--- static/js/conf.js
+++ static/js/conf.js
@@ -2,7 +2,7 @@ function addlinks(){
 
 
     var links = {
-    
+
         head:['/','/Hotellist','/Villalist','/Activelist',
               '/Other/index/id/36',//特别策划
               '/Other/index/id/37' //关于我们
@@ -12,18 +12,18 @@ function addlinks(){
               '/Hotellist',
               '/Villalist',
               '/Booknow',
-              
+
               '/Other/index/id/39', //公司信息
               '/Other/index/id/37',
               '/Other/index/id/40', //工作机会
-              '/Other/index/id/36', //新闻 41
-              
+              '/Other/index/id/41', //新闻 41
+
               '/Other/index/id/42', //帮助
               '/Other/index/id/43', //政策
               '/Other/index/id/44', //灾难响应
               '/Other/index/id/45' //条款与隐私
-              
-              
+
+
              ]
     }
     var d = document;
@@ -31,38 +31,40 @@ function addlinks(){
         footermenu_J = d.getElementById('footermenu_J'),
         special_J = d.getElementById('special_J'),
 		talkLink = d.getElementById('talkLink');
-    
+
     if(headmenu_J){
         var atag = headmenu_J.getElementsByTagName('A');
         for(var i= 0,j=atag.length;i<j;i++){
-            atag[i].href = links.head[i];        
+            atag[i].href = links.head[i];
         }
-    
+
     }
     if(footermenu_J){
         var atag = footermenu_J.getElementsByTagName('A');
         for(var i= 0,j=atag.length;i<j;i++){
-            atag[i].href = links.foot[i];        
+            if(atag[i].target!=='_blank'){
+                atag[i].href = links.foot[i];
+            }
         }
-    
+
     }
-    
+
     if(special_J){
         var atag = special_J.getElementsByTagName('A');
         for(var i= 0,j=atag.length;i<j;i++){
-            atag[i].href = links.cons[i];        
+            atag[i].href = links.cons[i];
         }
-    
+
     }
-	
+
 	if(talkLink){
-		
+
        talkLink.href = 'http://wpa.qq.com/msgrd?v=3&uin=2285722483&site=moonholidays.cn&menu=yes';
-    
+
     }
-	
-    
-        
+
+
+
 
 }
 
